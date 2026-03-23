ba58e40d0972fea78f96c5a66a31cba2fe4a0a4a
vicroja <vicroja@hotmail.com>
2026-03-23 09:26:05 -0500

    calculo del precio desglosado

--- app/Views/layouts/pms.php
+++ app/Views/layouts/pms.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>PMS - <?= session('tenant_name') ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= base_url('assets/css/modern-pastel.css') ?>?v=1.0">
 </head>
 <body class="bg-light">
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
